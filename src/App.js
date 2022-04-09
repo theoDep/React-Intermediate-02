40e2065e7b5afe69081f1c4258397698d2c75b21
theoDep <depelsemackertheo@yahoo.fr>
2022-04-09 15:37:41 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import { useState } from 'react';
 import './style.css';
 import axios from 'axios';
 
+const SampleQuote = {
+  quote: 'By chilling my loins I increase the chances of impregnating my wife.',
+  character: 'Apu Nahasapeemapetilon',
+  image:
+    'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629',
+  characterDirection: 'Left',
+};
+
 export default function App() {
   return (
     <div>
